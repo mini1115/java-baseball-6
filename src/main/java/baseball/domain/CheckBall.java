ace468d2644ed363bca024e9e076f7757d71723d
mini1115 <kmini9611@naver.com>
2023-10-29 20:04:42 +0900

    feat : 숫자의 인덱스가 일치하는지 판단하는 기능 추가

--- src/main/java/baseball/domain/CheckBall.java
+++ src/main/java/baseball/domain/CheckBall.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 public class CheckBall {
     //몇개의 숫자가 일치하는지
-    public int CorrectBallNumber(List<Integer> computer,List<Integer> player){
+    public int CorrectBallNumber(List<Integer> computer, List<Integer> player) {
         int result = 0;
-        for(int i=0;i<computer.size();i++){
-            if(computer.contains(player.get(i))){
-                result ++;
+        for (int i = 0; i < computer.size(); i++) {
+            if (computer.contains(player.get(i))) {
+                result++;
             }
         }
         return result;
+    }
+
+    public boolean samePlaceNumCheck(List<Integer> computer, int index, int number) {
+        return computer.get(index) == number;
     }
 }
